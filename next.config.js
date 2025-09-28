abab0fccb0f597bece7c25c25a57998abad44732
Sammyappiah <samuels12@hotmail.co.uk>
2025-09-28 22:45:41 +0300

    Force server build only, remove static export

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  experimental: {
-    typedRoutes: true,
-  },
+  output: "standalone", // ensure server build
   eslint: {
     ignoreDuringBuilds: true,
   },
